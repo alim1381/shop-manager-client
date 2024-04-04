7d0f0f8713a15bd0a50fd5e0b080c8cd2371d248
alim1381 <ali.moaydi2020@gmail.com>
2024-04-05 00:53:36 +0330

    sidebar closing with click

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -149,6 +149,7 @@ function Sidebar() {
               <Link
                 href={"/products/new"}
                 className="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+                onClick={() => setIsOpen(false)}
               >
                 New Product
                 <span className="ml-2" aria-hidden="true">
